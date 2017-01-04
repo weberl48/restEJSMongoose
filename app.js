623a9568be5871144593731f8643436233513334
weberl48 <weberl48@outlook.com>
2017-01-04 17:58:11 -0500

    show rest route added

--- app.js
+++ app.js
@@ -7,47 +7,62 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 //Schema Setup
-var campgroundSchema = new mongoose.Schema({name: String, image: String})
+var campgroundSchema = new mongoose.Schema({name: String, image: String, description: String})
 
 var Campground = mongoose.model("Campground", campgroundSchema)
+
+// Campground.create({name:'Alleganey', image:'http://www.photosforclass.com/download/6106475454', description: 'beautiful mountain views'})
+
 
 app.get('/', function(req, res) {
     res.render('landing')
 })
-
+//INDEX: SHOW ALL CAMPGROUNDS
 app.get('/campgrounds', function(req, res) {
     // Get all campgrounds from DBC
     Campground.find({}, function(err, campgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render('campground', {campgrounds: campgrounds})
+            res.render('index', {campgrounds: campgrounds})
 
         }
     })
 
 })
-
+//CREATEROUTE Add new campground to database
 app.post('/campgrounds', function(req, res) {
     var name = req.body.name
     var image = req.body.image
+    var description = req.body.description
     var newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     }
-    Campground.create(newCampground, function(err,newlyCreated) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(newlyCreated);
-        res.redirect('/campgrounds')
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect('/campgrounds')
 
-      }
+        }
     })
 })
 
 app.get('/campgrounds/new', function(req, res) {
     res.render('new')
+})
+
+app.get("/campgrounds/:id", function(req, res) {
+Campground.findById(req.params.id, function(err, foundCampground) {
+if (err) {
+  console.log(err);
+} else {
+  res.render('show', {campground: foundCampground});
+}
+})
 })
 app.listen(3000, function() {
     console.log('Server has started');
